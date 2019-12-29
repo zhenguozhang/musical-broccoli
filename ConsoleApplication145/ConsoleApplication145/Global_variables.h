33e088684a836573939855653514d8a47062cc3a
zhang zhenguo <1468171572@qq.com>
2019-12-30 02:10:40 +0800

    ubj

---
+++ ConsoleApplication145/ConsoleApplication145/Global_variables.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"pch.h"
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+extern ifstream fin;
+extern int * route_number;
--- Global_variables.cpp
+++
@@ -1,9 +0,0 @@
-#include"pch.h"
-#include<iostream>
-#include<string>
-#include<fstream>
-#include<iomanip>
-
-using namespace std;
-ifstream fin;
-int * route_number;
